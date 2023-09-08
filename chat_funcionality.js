360cfd04bf2bf3bfac28b90da6464c68d71ab96c
ofeka <ofekalm100@gmail.com>
2023-09-08 04:05:55 +0300

    Added function documentation

--- chat_funcionality.js
+++ chat_funcionality.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Extracts text after a '!act' command from the given input.
+ *
+ * @param {string} input - The input string to search for '!act' commands.
+ * @returns {string | null} - The extracted text after the '!act' command or null if not found.
+ */
 function extractTextAfterAct(input) {
     const match = input.match(/!act\s*(.+)/);
     if (match && match[1]) {
@@ -9,11 +15,23 @@ function extractTextAfterAct(input) {
     return null; // Return null if there's no match or text after "!act"
   }
 
+/**
+ * Retrieves the chat history from a JSON file.
+ *
+ * @param {string} fileName - The name of the JSON file to read.
+ * @returns {Array} - An array containing chat history.
+ */
 function getChatHistory(fileName){
     const data = JSON.parse(fs.readFileSync(fileName, 'utf-8'));
     return data.history;
 }
 
+/**
+ * Creates or retrieves the chat history from a JSON file.
+ *
+ * @param {string} filename - The name of the JSON file to create or retrieve.
+ * @returns {Array} - An array containing chat history.
+ */
 async function createChatHistoryOrRetrieve(filename) {
     const filePath = path.resolve(filename);
     if(fs.existsSync(filePath)){
@@ -25,6 +43,13 @@ async function createChatHistoryOrRetrieve(filename) {
         return getChatHistory(filePath);
     }
 }
+
+/**
+ * Writes a message to the chat history in a JSON file.
+ *
+ * @param {string} filePath - The path to the JSON file to write the message to.
+ * @param {string} message - The message to add to the chat history.
+ */
 function writeChatHistory(filePath, message){
     const loadedData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
     loadedData.history.push(message);
@@ -33,14 +58,15 @@ function writeChatHistory(filePath, message){
     fs.writeFileSync(filePath,jsonData,'utf-8');
 }
 
-//remove the whatsapp pattern in order
-//to make a json file for that Chat/groupChat.
-//input example - 
-//----------@c.us
-//----------@g.us
-//output example -
-//----------
-//----------
+/**
+ * Removes specific patterns from the input text.
+ * for modifying the Whatsapp chatID response to create the JSON.
+ * before: 120363159238258722@c.us
+ * before: 120363159238258722@g.us
+ * after:  120363159238258722
+ * @param {string} inputText - The text from which patterns should be removed.
+ * @returns {string} - The input text with specified patterns removed.
+ */
 function removePattern(inputText) {
     return inputText.replace(/@g\.us|@c\.us/g, '');
 }
--- open_ai_gpt.js
+++ open_ai_gpt.js
@@ -16,6 +16,12 @@ const configuration = {
 
 const openai = new openAI(configuration);
 
+/**
+ * Asks the GPT-3.5 Turbo model a question or provides a prompt for generating text.
+ *
+ * @async
+ * @returns {Promise<string>} - A Promise that resolves to the generated response text.
+ */
 async function askGPT(){
     const chatCompletion = await openai.chat.completions.create({
         messages: messages,
@@ -23,6 +29,7 @@ async function askGPT(){
     });
     return chatCompletion.choices[0].message.content;
 }
+
 module.exports = {
     askGPT,
     chatHistory,
